eb6c6b92fddec63dcf6ba3b66c624f58c9d02abe
Manish Sharma <gautamanesh2469@gmail.com>
2026-06-09 09:14:35 +0530

    Print array elements using recursion concept

--- Recursion/printinarray.cpp
+++ Recursion/printinarray.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 void PrintArr(int arr[], int index, int n){
-    if(index == n){
+    if(index == n){//Base condition
         return;
     }
     cout<<arr[index]<<" ";
